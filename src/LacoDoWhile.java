085aff594b7cbb2ffdbe5f11b74ccbff3efe9759
Adelmo <adelmom@uol.com.br>
2023-05-17 21:38:00 -0300

    Aula de Lacos

---
+++ src/LacoDoWhile.java
@@ -0,0 +1,11 @@
+
+public class LacoDoWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	//	do while
+
+	}
+
+}
---
+++ src/LacoFor.java
@@ -0,0 +1,11 @@
+
+public class LacoFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//for
+	}
+
+}
